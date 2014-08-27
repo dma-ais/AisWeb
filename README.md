6e478574df0574348dcde062f60f4b3974f99e4e
Ole Borup <obo@dma.dk>
2014-08-27 15:02:34 +0200

    Initial web application with multiple run options

--- README.md
+++ README.md
@@ -2,3 +2,45 @@ AisWeb
 ======
 
 Web portal for various AIS services
+
+## Prerequisites
+
+* Java 8
+* Maven 3
+
+## Building
+
+	mvn clean install
+	
+## Running
+
+Multiple choices for running the web app
+
+### Run Jetty with maven
+
+	mvn jetty:run
+	
+The maven plugin scans for changes so very suited for web development. Access the app at
+
+	http://localhost:8080/
+	
+### Run standalone WAR with embedded Jetty
+
+	java -jar target/ais-web-0.1-SNAPSHOT-standalone.war
+	
+Access the app at
+
+	http://localhost:8080/
+	
+### Run from IDE
+
+Run `dk.dma.ais.web.app.AisWebApp` from inside Eclipse, IDEA, etc.
+
+### Deploy on servlet container
+
+Deploy WAR file on Tomcat, JBoss, Jetty, etc.  
+
+## License ##
+
+This library is licensed under the Apache License, Version 2.0.
+
---
+++ src/main/java/dk/dma/ais/web/app/AisWebApp.java
@@ -0,0 +1,60 @@
+/* Copyright (c) 2011 Danish Maritime Authority.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dk.dma.ais.web.app;
+
+import java.io.File;
+import java.lang.Thread.UncaughtExceptionHandler;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.beust.jcommander.Parameter;
+import com.google.inject.Injector;
+
+import dk.dma.commons.app.AbstractDaemon;
+
+public class AisWebApp extends AbstractDaemon {
+
+    static final Logger LOG = LoggerFactory.getLogger(AisWebApp.class);
+    
+    @Parameter(names = "-port", description = "The port to run AisWeb on")
+    int port = 8080;
+
+    @Override
+    protected void runDaemon(Injector injector) throws Exception {
+        final Server server = new Server(port);
+        ((ServerConnector) server.getConnectors()[0]).setReuseAddress(true);
+        final File webappDir = new File(AisWebApp.class.getProtectionDomain().getCodeSource().getLocation().getFile());
+        final WebAppContext webappContext = new WebAppContext(webappDir.getAbsolutePath(), "/");
+        server.setHandler(webappContext);
+        server.start();
+        server.join();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                LOG.error("Uncaught exception in thread " + t.getClass().getCanonicalName() + ": " + e.getMessage(), e);
+                System.exit(-1);
+            }
+        });
+        new AisWebApp().execute(args);
+    }
+
+}
