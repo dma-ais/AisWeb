179c7bce2c21d77b4c696d5d800b137745ff7e2c
Jens Tuxen <mail@jenstuxen.com>
2014-09-01 19:44:54 +0200

    Initial add for AisStore Downloader gui (AISVIEW-34, AISWEB-1)

---
+++ src/main/webapp/downloader/README.md
@@ -0,0 +1,7 @@
+Deployment details
+==================
+
+* The webapp is self contained
+* Only setup is to change the string variable 'requestDomain' in js/data.js line 6 to server domain
+* quick deployment test can be done using (cd webapp; python -m SimpleHTTPServer)
+
---
+++ src/main/webapp/downloader/js/application.js
@@ -0,0 +1,25 @@
+// scrollspy functionality
+//TODO: move to some other file
+
+!function ($) {
+
+  $(function(){
+
+    var $window = $(window)
+    var $body   = $(document.body)
+
+    var navHeight = $('.navbar').outerHeight(true) + 10
+
+    $body.scrollspy({
+      target: '.bs-sidebar',
+      offset: navHeight
+    })
+
+    $window.on('load', function () {
+      $body.scrollspy('refresh')
+    })
+})
+
+}(window.jQuery)
+
+
