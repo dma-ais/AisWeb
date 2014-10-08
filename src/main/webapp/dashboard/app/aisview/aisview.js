c765fcca4427bbb25696ee2947d7918f51c9e609
Thomas Borg Salling <tbsalling@tbsalling.dk>
2014-10-08 11:26:21 +0200

    Added OpenLayers via bower

--- src/main/webapp/dashboard/app/aisview/aisview.js
+++ src/main/webapp/dashboard/app/aisview/aisview.js
@@ -11,8 +11,7 @@ angular.module('dashboardApp.aisview', ['ui.chart', 'google-maps'])
                 rendererOptions: {
                     // Put data labels on the pie slices.
                     // By default, labels show the percentage of the slice.
-                    showDataLabels: true,
-
+                    showDataLabels: true
                 }
             },
             legend: {
@@ -176,6 +175,8 @@ angular.module('dashboardApp.aisview', ['ui.chart', 'google-maps'])
             },
             zoom: 8
         };
+
+        drawOpenSeaMap();
     });
 
 //works when requesting just one column
@@ -212,4 +213,70 @@ function resetGraphs(graphs) {
             ['PLACEHOLDER', 1]
         ];
     });
+}
+
+function drawOpenSeaMap() {
+    var map = new OpenLayers.Map("openlayers-map", {
+        projection: new OpenLayers.Projection("EPSG:900913"),
+        displayProjection: new OpenLayers.Projection("EPSG:4326"),
+        controls: [
+            new OpenLayers.Control.Navigation(),
+            new OpenLayers.Control.ScaleLine({topOutUnits : "nmi", bottomOutUnits: "km", topInUnits: 'nmi', bottomInUnits: 'km', maxWidth: '40'}),
+            //new OpenLayers.Control.LayerSwitcher(),
+            new OpenLayers.Control.MousePosition(),
+            new OpenLayers.Control.PanZoomBar()],
+        //numZoomLevels: 18,
+        //maxResolution: 156543,
+        units: 'meters'
+    });
+
+    map.addLayer(new OpenLayers.Layer.OSM());
+
+    map.setCenter(new OpenLayers.LonLat(12.0, 56.0).transform(new OpenLayers.Projection("EPSG:4326"),new OpenLayers.Projection("EPSG:900913")));
+    map.zoomTo(5);
+
+    /*
+    // Position and zoomlevel of the map
+    var lon = 12.0915;
+    var lat = 54.1878;
+    var zoom = 15;
+
+    var map = new OpenLayers.Map('openlayers-map', {
+        projection: new OpenLayers.Projection("EPSG:900913"),
+        displayProjection: new OpenLayers.Projection("EPSG:4326"),
+        controls: [
+            new OpenLayers.Control.Navigation(),
+            new OpenLayers.Control.ScaleLine({topOutUnits : "nmi", bottomOutUnits: "km", topInUnits: 'nmi', bottomInUnits: 'km', maxWidth: '40'}),
+            new OpenLayers.Control.LayerSwitcher(),
+            new OpenLayers.Control.MousePosition(),
+            new OpenLayers.Control.PanZoomBar()],
+        maxExtent:
+            new OpenLayers.Bounds(-20037508.34, -20037508.34, 20037508.34, 20037508.34),
+        numZoomLevels: 18,
+        maxResolution: 156543,
+        units: 'meters'
+    });
+/*
+    // set the map centre on the middle of Denmark
+    var lat = 56;
+    var lon = 12;
+    var zoom = 7;
+    map.setCenter(new OpenLayers.LonLat(lon, lat).transform(new OpenLayers.Projection("EPSG:4326"),new OpenLayers.Projection("EPSG:900913"), zoom)
+*
+    var layer_default = new OpenLayers.Layer.OSM();
+
+    // Mapnik
+//    var layer_mapnik = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
+    // Osmarender
+    // XXX osmarender gibt es nicht mehr
+    // layer_tah = new OpenLayers.Layer.OSM.Osmarender("Osmarender");
+    // Seamark
+ //   var layer_seamark = new OpenLayers.Layer.TMS("Seezeichen", "http://tiles.openseamap.org/seamark/",
+ //       { numZoomLevels: 18, type: 'png', getURL: getTileURL, isBaseLayer: false, displayOutsideMaxExtent: true});
+
+    map.addLayers([layer_default]);
+//    jumpTo(lon, lat, zoom);
+
+    // Add harbour layer
+    //init_haefen(map, "./map/"); */
 }
