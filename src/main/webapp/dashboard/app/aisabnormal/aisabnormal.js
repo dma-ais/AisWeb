69c889a42bd4cf08273e58cde5d4396d74114b12
Thomas Borg Salling <tbsalling@tbsalling.dk>
2014-10-09 10:19:42 +0200

    Move abnormal UI-portions to separate module.

---
+++ src/main/webapp/dashboard/app/aisabnormal/aisabnormal.js
@@ -0,0 +1,73 @@
+'use strict';
+
+angular.module('dashboardApp.aisabnormal', ['google-maps'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/aisabnormal', {
+            templateUrl: 'aisabnormal/aisabnormal.html',
+            controller: 'AisAbnormalCtrl'
+        });
+    }])
+
+    .controller('AisAbnormalCtrl', function ($scope) {
+        $scope.map = {
+            center: {
+                latitude: 56,
+                longitude: 12
+            },
+            zoom: 8
+        };
+
+        drawOpenSeaMap();
+    });
+
+var openLayersMap;
+
+function drawOpenSeaMap() {
+    openLayersMap = new OpenLayers.Map("openlayers-map", {
+        projection: new OpenLayers.Projection("EPSG:900913"),
+        displayProjection: new OpenLayers.Projection("EPSG:4326"),
+        controls: [
+            new OpenLayers.Control.Navigation(),
+            new OpenLayers.Control.ScaleLine({topOutUnits: "nmi", bottomOutUnits: "km", topInUnits: 'nmi', bottomInUnits: 'km', maxWidth: '40'}),
+            //new OpenLayers.Control.LayerSwitcher(),
+            new OpenLayers.Control.MousePosition(),
+            new OpenLayers.Control.PanZoomBar()],
+        //numZoomLevels: 18,
+        maxResolution: 156543,
+        units: 'meters'
+    });
+
+    openLayersMap.addLayer(new OpenLayers.Layer.OSM());
+
+    openLayersMap.setCenter(new OpenLayers.LonLat(12.0, 56.0).transform(new OpenLayers.Projection("EPSG:4326"), new OpenLayers.Projection("EPSG:900913")));
+    openLayersMap.zoomTo(6);
+
+    // Add OpenSeaMap layers - http://www.openseamap.org/
+    // Seamark
+    var layer_seamark = new OpenLayers.Layer.TMS("Seezeichen", "http://tiles.openseamap.org/seamark/", { numZoomLevels: 18, type: 'png', getURL: getTileURL, isBaseLayer: false, displayOutsideMaxExtent: true});
+    // Harbours
+    //var layer_harbours = new OpenLayers.Layer.Markers("Häfen", { projection: new OpenLayers.Projection("EPSG:4326"), visibility: true, displayOutsideMaxExtent:true});
+    //layer_harbours.setOpacity(0.8);
+
+    openLayersMap.addLayers([layer_seamark /* layer_harbours*/]);
+}
+
+function getTileURL(bounds) {
+    var res = openLayersMap.getResolution();
+    var x = Math.round((bounds.left - openLayersMap.maxExtent.left) / (res * 256 /*this.tileSize.w */));
+    var y = Math.round((openLayersMap.maxExtent.top - bounds.top) / (res * 256 /*this.tileSize.h */));
+    var z = openLayersMap.getZoom();
+    var limit = Math.pow(2, z);
+    if (y < 0 || y >= limit) {
+        return null;
+    } else {
+        x = ((x % limit) + limit) % limit;
+        var url = this.url;
+        var path = z + "/" + x + "/" + y + "." + this.type;
+        if (url instanceof Array) {
+            url = this.selectUrl(path, url);
+        }
+        return url + path;
+    }
+}
--- src/main/webapp/dashboard/app/app.js
+++ src/main/webapp/dashboard/app/app.js
@@ -2,27 +2,23 @@
 
 // Declare app level module which depends on views, and components
 var app = angular.module('dashboardApp', [
-  'ngRoute',
-  'ui.bootstrap',
-  'ui.chart',
-  'google-maps',
-  'dashboardApp.aisview',
-  'dashboardApp.aisstore',
-  'dashboardApp.version'
+    'ngRoute',
+    'ui.bootstrap',
+    'ui.chart',
+    'google-maps',
+    'dashboardApp.aisview',
+    'dashboardApp.aisstore',
+    'dashboardApp.aisabnormal',
+    'dashboardApp.version'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/'});
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/'});
+    }]);
+
+app.controller("DashboardCtrl", ['$scope', function ($scope) {
+    $scope.views = [
+        {name: 'AisStore', url: 'aisstore/aisstore.html'},
+        {name: 'AisView', url: 'aisview/aisview.html'},
+        {name: 'AisAbnormal', url: 'aisabnormal/aisabnormal.html'}
+    ];
 }]);
-
-
-app.controller("DashboardCtrl", ['$scope', function($scope) {
-	$scope.views = [
-		{name: 'AisStore', url: 'aisstore/aisstore.html'},
-		{name: 'AisView', url: 'aisview/aisview.html'},
-	];
-
-
-}]);
-
-
-
